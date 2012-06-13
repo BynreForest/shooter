84f0844aeaab0150a1bc5cbe9b7389422d76d5da
Brendan Gaynor <gaynorbrendan@gmail.com>
2012-06-13 12:49:51 -0400

    shooting added

--- shooter/HelloWorldLayer.m
+++ shooter/HelloWorldLayer.m
@@ -33,21 +33,105 @@
 {
 	// always call "super" init
 	// Apple recommends to re-assign "self" with the "super" return value
-	if( (self=[super init])) {
+	if( (self=[super initWithColor:ccc4(255,255,255,255)] )) {
 		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+		CGSize winSize = [[CCDirector sharedDirector] winSize];
+        CCSprite *player = [CCSprite spriteWithFile:@"Player.png" 
+                                               rect:CGRectMake(0, 0, 27, 40)];
+        player.position = ccp(player.contentSize.width/2, winSize.height/2);
+        [self addChild:player];	
+        
+        [self schedule:@selector(gameLogic:) interval:1.0];
+        
+        self.isTouchEnabled = YES;
 
-		// ask director the the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
 	}
 	return self;
+}
+
+-(void)gameLogic:(ccTime)dt {
+    [self addTarget];
+}
+
+-(void)spriteMoveFinished:(id)sender {
+    CCSprite *sprite = (CCSprite *)sender;
+    [self removeChild:sprite cleanup:YES];
+}
+
+-(void)addTarget {
+    
+    CCSprite *target = [CCSprite spriteWithFile:@"Target.png" 
+                                           rect:CGRectMake(0, 0, 27, 40)]; 
+    
+    // Determine where to spawn the target along the Y axis
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    int minY = target.contentSize.height/2;
+    int maxY = winSize.height - target.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create the target slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    target.position = ccp(winSize.width + (target.contentSize.width/2), actualY);
+    [self addChild:target];
+    
+    // Determine speed of the target
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // Create the actions
+    id actionMove = [CCMoveTo actionWithDuration:actualDuration 
+                                        position:ccp(-target.contentSize.width/2, actualY)];
+    id actionMoveDone = [CCCallFuncN actionWithTarget:self 
+                                             selector:@selector(spriteMoveFinished:)];
+    [target runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    // Choose one of the touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:[touch view]];
+    location = [[CCDirector sharedDirector] convertToGL:location];
+    
+    // Set up initial location of projectile
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    CCSprite *projectile = [CCSprite spriteWithFile:@"Projectile.png" 
+                                               rect:CGRectMake(0, 0, 20, 20)];
+    projectile.position = ccp(20, winSize.height/2);
+    
+    // Determine offset of location to projectile
+    int offX = location.x - projectile.position.x;
+    int offY = location.y - projectile.position.y;
+    
+    // Bail out if we are shooting down or backwards
+    if (offX <= 0) return;
+    
+    // Ok to add now - we've double checked position
+    [self addChild:projectile];
+    
+    // Determine where we wish to shoot the projectile to
+    int realX = winSize.width + (projectile.contentSize.width/2);
+    float ratio = (float) offY / (float) offX;
+    int realY = (realX * ratio) + projectile.position.y;
+    CGPoint realDest = ccp(realX, realY);
+    
+    // Determine the length of how far we're shooting
+    int offRealX = realX - projectile.position.x;
+    int offRealY = realY - projectile.position.y;
+    float length = sqrtf((offRealX*offRealX)+(offRealY*offRealY));
+    float velocity = 480/1; // 480pixels/1sec
+    float realMoveDuration = length/velocity;
+    
+    // Move projectile to actual endpoint
+    [projectile runAction:[CCSequence actions:
+                           [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
+                           [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
+                           nil]];
+    
 }
 
 // on "dealloc" you need to release all your retained objects
