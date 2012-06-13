8ebc53998d032c14b89181308c23a2775e263331
Brendan Gaynor <gaynorbrendan@gmail.com>
2012-06-13 09:34:55 -0400

    Initial Commit

---
+++ shooter/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  shooter
+//
+//  Created by Brendan Gaynor on 6/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ shooter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  shooter
+//
+//  Created by Brendan Gaynor on 6/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
